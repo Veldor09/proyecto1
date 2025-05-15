a095962f0e08ffe3bffa1559d7aa60341a6bace4
AshVargas <vargasashly260@gmail.com>
2025-05-14 23:10:09 -0600

    cambios en el home

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,13 +1,22 @@
-import { Outlet } from "@tanstack/react-router";
+import { Outlet, useRouterState } from "@tanstack/react-router";
 import NavBar from "./NavBar";
 
 const Layout = () => {
+  const { location } = useRouterState();
+
+  // Si estás en la ruta / (homepage), no encierres en container
+  const isHome = location.pathname === "/";
+
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <NavBar />
-      <div className="container mx-auto px-4 py-8">
-        <Outlet />
-      </div>
+      {isHome ? (
+        <Outlet /> // Home se renderiza a pantalla completa
+      ) : (
+        <div className="container mx-auto px-4 py-8">
+          <Outlet />
+        </div>
+      )}
     </div>
   );
 };
