76454232a1d1365fcc698fee0f589e5f35d39caa
Sam2999 <albertsam29@gmail.com>
2025-05-15 01:29:41 -0600

    Edit delete aliados

--- src/Components/Aliados/AddAliadoButton.tsx
+++ src/Components/Aliados/AddAliadoButton.tsx
@@ -9,7 +9,7 @@ const AddAliadoButton = () => {
     <>
       <button
         onClick={() => setIsModalOpen(true)}
-        className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700 transition"
+        className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition"
       >
         + Agregar Aliado
       </button>
